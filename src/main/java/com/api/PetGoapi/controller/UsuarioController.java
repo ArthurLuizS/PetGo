da9a5828cc54262ea1908cc662f2afbc72553caa
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-11 18:54:08 -0300

    Primeiro mapeamento post de cadastro de cliente

--- src/main/java/com/api/PetGoapi/controller/UsuarioController.java
+++ src/main/java/com/api/PetGoapi/controller/UsuarioController.java
@@ -1,15 +1,18 @@
 package com.api.PetGoapi.controller;
 
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.PetGoapi.ClienteAssembler;
 import com.api.PetGoapi.model.Cliente;
-import com.api.PetGoapi.model.Endereco;
-import com.api.PetGoapi.model.Funcionario;
-import com.api.PetGoapi.model.Usuario;
+import com.api.PetGoapi.model.input.ClienteInput;
+import com.api.PetGoapi.service.ClienteService;
 
 import lombok.AllArgsConstructor;
 
@@ -17,18 +20,14 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/")
 public class UsuarioController {
-	
+	@Autowired
 	private ClienteAssembler clienteAssembler;
+	private ClienteService clienteService;
 	
-	
-	@GetMapping("usuarios")
-	public ResponseEntity<Cliente> index() {
-		Cliente novocliente = new Cliente("Arthur", "Arthur@email");
-		novocliente.setNome("juan");
-		novocliente.setNumeroconta("11111");
-		
-		
-		
-		return ResponseEntity.ok(novocliente);
+	@PostMapping
+	private ResponseEntity<Cliente> cadastrar(@RequestBody ClienteInput clienteInput){
+		Cliente novoCliente = clienteAssembler.toEntity(clienteInput);
+		novoCliente.setDataCliente(OffsetDateTime.now());
+		return ResponseEntity.ok(clienteService.save(novoCliente));
 	}
 }
