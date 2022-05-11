4d452c6f9b754d77f3e854bc28e5350aad0c00d9
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-11 18:57:25 -0300

    Service de cliente

--- src/main/java/com/api/PetGoapi/service/ClienteService.java
+++ src/main/java/com/api/PetGoapi/service/ClienteService.java
@@ -1,5 +1,20 @@
 package com.api.PetGoapi.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.api.PetGoapi.model.Cliente;
+import com.api.PetGoapi.repository.ClienteRepository;
+
+@Service
 public class ClienteService {
 
+	private ClienteRepository clienteRepository;
+	
+	@Transactional
+	public Cliente save(Cliente novoCliente) {
+		
+		return clienteRepository.save(novoCliente);
+	}
 }
