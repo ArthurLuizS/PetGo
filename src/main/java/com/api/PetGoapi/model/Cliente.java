e0132071aa7b76fa6dac2995c1375fd731927de6
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-11 18:55:02 -0300

    Dados iniciais do cliente

--- src/main/java/com/api/PetGoapi/model/Cliente.java
+++ src/main/java/com/api/PetGoapi/model/Cliente.java
@@ -1,17 +1,28 @@
 package com.api.PetGoapi.model;
 
-import javax.persistence.Embedded;
+import java.time.OffsetDateTime;
 
-import lombok.Builder;
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Cliente{
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private OffsetDateTime dataCliente;
 	private String nome;
 	private String telefone;
 	private String email;
+	private String login;
 	private String senha;
 	@Embedded
 	private Endereco endereco;
