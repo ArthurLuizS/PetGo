f6066c37ebc5c103ac25ec9a33e0d3d0c0a689b2
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-11 18:56:36 -0300

    Repositorio jpa de cliente

--- src/main/java/com/api/PetGoapi/repository/ClienteRepository.java
+++ src/main/java/com/api/PetGoapi/repository/ClienteRepository.java
@@ -1,5 +1,9 @@
 package com.api.PetGoapi.repository;
 
-public interface ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.PetGoapi.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
