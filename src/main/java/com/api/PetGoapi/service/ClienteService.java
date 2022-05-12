dddb6004d2b824b97e0ec77bbb17c7cc01d4789d
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-12 18:13:04 -0300

    Anotacao do contructor e adicao do buscar

--- src/main/java/com/api/PetGoapi/service/ClienteService.java
+++ src/main/java/com/api/PetGoapi/service/ClienteService.java
@@ -1,5 +1,7 @@
 package com.api.PetGoapi.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Service;
 import com.api.PetGoapi.model.Cliente;
 import com.api.PetGoapi.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class ClienteService {
 
 	private ClienteRepository clienteRepository;
@@ -16,5 +21,10 @@ public class ClienteService {
 	public Cliente save(Cliente novoCliente) {
 		
 		return clienteRepository.save(novoCliente);
+	}
+
+	public List<Cliente> listar() {
+	
+		return clienteRepository.findAll();
 	}
 }
