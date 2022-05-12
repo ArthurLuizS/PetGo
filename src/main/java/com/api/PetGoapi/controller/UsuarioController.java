fe2f17dc7cfab64ec13d084c351aabe23ca52382
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-12 18:08:55 -0300

    Metodo post e get adicionado

--- src/main/java/com/api/PetGoapi/controller/UsuarioController.java
+++ src/main/java/com/api/PetGoapi/controller/UsuarioController.java
@@ -1,33 +1,42 @@
 package com.api.PetGoapi.controller;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.PetGoapi.ClienteAssembler;
+//import com.api.PetGoapi.Assembler.ClienteAssembler;
 import com.api.PetGoapi.model.Cliente;
 import com.api.PetGoapi.model.input.ClienteInput;
 import com.api.PetGoapi.service.ClienteService;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/")
 public class UsuarioController {
-	@Autowired
-	private ClienteAssembler clienteAssembler;
+	
+	//private ClienteAssembler clienteAssembler;
+	
 	private ClienteService clienteService;
 	
 	@PostMapping
-	private ResponseEntity<Cliente> cadastrar(@RequestBody ClienteInput clienteInput){
-		Cliente novoCliente = clienteAssembler.toEntity(clienteInput);
-		novoCliente.setDataCliente(OffsetDateTime.now());
-		return ResponseEntity.ok(clienteService.save(novoCliente));
+	public ResponseEntity<Cliente> cadastrar(@RequestBody Cliente cliente /*ClienteInput clienteInput*/){
+	//	Cliente novoCliente = clienteAssembler.toEntity(clienteInput);
+		
+		cliente.setDataCliente(OffsetDateTime.now());
+		return ResponseEntity.ok(clienteService.save(cliente));
+	}
+	@GetMapping("/usuarios")
+	public ResponseEntity<List<Cliente>> listar(){
+		return ResponseEntity.ok(clienteService.listar());
 	}
 }
