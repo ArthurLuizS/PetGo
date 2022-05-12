c486991054b236211e8eaa7a84b0cef3910b42df
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-12 18:10:46 -0300

    Anotacao repository

--- src/main/java/com/api/PetGoapi/repository/ClienteRepository.java
+++ src/main/java/com/api/PetGoapi/repository/ClienteRepository.java
@@ -1,9 +1,11 @@
 package com.api.PetGoapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.api.PetGoapi.model.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
