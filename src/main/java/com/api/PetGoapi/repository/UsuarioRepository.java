c089a86cc8697997499919ef81141d9092ae2d50
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-18 19:18:42 -0300

    Interface usuario repository

---
+++ src/main/java/com/api/PetGoapi/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.api.PetGoapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.PetGoapi.model.Usuario;
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
