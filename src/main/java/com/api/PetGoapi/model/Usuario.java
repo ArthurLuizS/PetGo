81980ec2892fcf773d3b3d43bfb9128bb0bc01e2
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-18 19:16:16 -0300

    configurando class pai usuario

--- src/main/java/com/api/PetGoapi/model/Usuario.java
+++ src/main/java/com/api/PetGoapi/model/Usuario.java
@@ -1,17 +1,35 @@
 package com.api.PetGoapi.model;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+@Accessors(chain = true)
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@DiscriminatorColumn(name = "DTYPE")
 public class Usuario {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String telefone;
+	private String login;
 	private String email;
 	private String senha;
 	@Embedded
