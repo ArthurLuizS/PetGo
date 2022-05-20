b558767533f9a4161cce7c6a8eaee3ec6d0334c9
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-20 20:34:52 -0300

    Modelos animal e endereco alterados

--- src/main/java/com/api/PetGoapi/model/Animal.java
+++ src/main/java/com/api/PetGoapi/model/Animal.java
@@ -2,15 +2,37 @@ package com.api.PetGoapi.model;
 
 import java.time.OffsetDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@Entity
+@EqualsAndHashCode
 public class Animal {
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@ManyToOne
+	private Cliente cliente;
+	@Column(name = "nome_animal")
 	private String nome;
+	@Column(name = "tipo_animal")
+	private String tipoAnimal;
+	@Column(name = "data_nascimento")
 	private OffsetDateTime dataNascimento;
+	@Column(name = "peso_animal")
 	private Float peso;
+	@Column(name = "porte_animal")
 	private String porte;
 	private String sexo;
-	private String observaçoes;
+	private String observacoes;
+	private String raca;
 }
--- src/main/java/com/api/PetGoapi/model/Endereco.java
+++ src/main/java/com/api/PetGoapi/model/Endereco.java
@@ -1,15 +1,26 @@
 package com.api.PetGoapi.model;
 
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Embeddable
+@Entity
+@EqualsAndHashCode
 public class Endereco {
-
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	private Cliente cliente;
 	private String rua;
 	private String numero;
 	private String cep;
