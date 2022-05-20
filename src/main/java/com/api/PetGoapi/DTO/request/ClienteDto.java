e3985341cf559dd1560466722cb635ce2e0842d3
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-20 20:31:52 -0300

    cliente dto

--- src/main/java/com/api/PetGoapi/DTO/request/ClienteDto.java
+++ src/main/java/com/api/PetGoapi/DTO/request/ClienteDto.java
@@ -1,7 +1,8 @@
 package com.api.PetGoapi.DTO.request;
 
-import java.time.OffsetDateTime;
+import java.util.List;
 
+import com.api.PetGoapi.model.Animal;
 import com.api.PetGoapi.model.Cliente;
 import com.api.PetGoapi.model.Endereco;
 
@@ -20,17 +21,38 @@ public class ClienteDto {
 	private String login;
 	private String senha;
 	private String confirmaSenha;
-	private Endereco endereco;
+	private EnderecoDtoRequest endereco;
+	private List<Animal> animais;
+
 	
 	public Cliente build() {
-		Cliente cliente = new Cliente().setNome(this.nome)
-										.setDataCliente(OffsetDateTime.now())
-										.setCpf(this.cpf)
+		Cliente cliente = new Cliente(); 
+		cliente.setNome(this.nome);
+										//.setDataCliente(OffsetDateTime.now())
+		cliente.setCpf(this.cpf)
 										.setEmail(this.email)
 										.setLogin(this.login)
 										.setSenha(this.senha)
 										.setTelefone(this.telefone)
-										.setEndereco(endereco);
-		return cliente;
+								//		.setEndereco(endereco);
+										;
+		return cliente;//.setAnimais(this.animais);
 	}
+	
+	public Animal buildAnimal(Animal animal) {
+		Animal animalnovo = new Animal();
+		animalnovo.setDataNascimento(animal.getDataNascimento());
+		animalnovo.setNome(animal.getNome());
+		animalnovo.setObservacoes(animal.getObservacoes());
+		animalnovo.setPeso(animal.getPeso());
+		animalnovo.setPorte(animal.getPorte());
+		animalnovo.setRaca(animal.getRaca());
+		animalnovo.setSexo(animal.getSexo());
+		
+		return animalnovo;
+		
+	}
+	
+	
+	
 }
