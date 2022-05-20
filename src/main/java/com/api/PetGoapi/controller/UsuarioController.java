b60a7566e156ebd7e7b8ca249600397c3e3505f3
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-20 20:33:33 -0300

    Setando os dados

--- src/main/java/com/api/PetGoapi/controller/UsuarioController.java
+++ src/main/java/com/api/PetGoapi/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.api.PetGoapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.PetGoapi.DTO.request.ClienteDto;
-import com.api.PetGoapi.DTO.response.ClienteResponseDto;
+import com.api.PetGoapi.DTO.request.EnderecoDtoRequest;
+import com.api.PetGoapi.model.Animal;
 //import com.api.PetGoapi.Assembler.ClienteAssembler;
 import com.api.PetGoapi.model.Cliente;
+import com.api.PetGoapi.model.Endereco;
+import com.api.PetGoapi.service.AnimalService;
 import com.api.PetGoapi.service.ClienteService;
+import com.api.PetGoapi.service.EnderecoService;
 
 import lombok.AllArgsConstructor;
 
@@ -27,21 +32,27 @@ public class UsuarioController {
 	//private ClienteAssembler clienteAssembler;
 	@Autowired
 	private ClienteService clienteService;
+	private AnimalService animalService;
+	private EnderecoService enderecoService;
+	//private EnderecoDtoRequest enderecoDtoRequest;
 	
 	@PostMapping
-	public ResponseEntity<ClienteResponseDto> cadastrar(@RequestBody ClienteDto clienteDto /*ClienteInput clienteInput*/){
-		//Cliente novoCliente = clienteAssembler.toEntity(clienteDto);
-	//	novoCliente.setDataCliente(OffsetDateTime.now());
-	//	clienteInput.setDataCliente(OffsetDateTime.now());
-		
-	//	cliente.setDataCliente(OffsetDateTime.now());
-		
-
+	public ResponseEntity<Cliente> cadastrar(@RequestBody ClienteDto clienteDto /*ClienteInput clienteInput*/){
 		
 		if(!clienteDto.getSenha().contentEquals(clienteDto.getConfirmaSenha())) {
 			return ResponseEntity.badRequest().build();
 		}
-		return ResponseEntity.ok(clienteService.save(clienteDto.build()));
+		Cliente clienteSalvo = clienteService.save(clienteDto.build()) ;
+		Animal animalSalvo = clienteDto.buildAnimal(clienteDto.getAnimais().get(0));
+		animalSalvo.setCliente(clienteSalvo);
+		animalService.cadastrar(animalSalvo);
+		EnderecoDtoRequest enderecoDtoRequest = new EnderecoDtoRequest();
+		Endereco enderecoSalvo = enderecoDtoRequest.build(clienteDto.getEndereco());
+		enderecoSalvo.setCliente(clienteSalvo);
+		enderecoService.cadastrar(enderecoSalvo);
+		
+		
+		return ResponseEntity.ok(clienteSalvo);
 	}
 	@GetMapping("/usuarios")
 	public ResponseEntity<List<Cliente>> listar(){
@@ -49,7 +60,7 @@ public class UsuarioController {
 	}
 	
 	@GetMapping("{clienteId}")
-	public ResponseEntity<ClienteResponseDto> buscar(@PathVariable Long clienteId){
+	public ResponseEntity<Optional<Cliente>> buscar(@PathVariable Long clienteId){
 		
 		return ResponseEntity.ok(clienteService.buscar(clienteId));
 	}
