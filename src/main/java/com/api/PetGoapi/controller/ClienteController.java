8e92036f0945ec4c4f1e84917a3a392f0a6fd3fc
Arthur Luiz <arthurluiz36@gmail.com>
2022-05-26 20:55:33 -0300

    controler utilizando as classe cliente input

--- src/main/java/com/api/PetGoapi/controller/ClienteController.java
+++ src/main/java/com/api/PetGoapi/controller/ClienteController.java
@@ -1,7 +1,6 @@
 package com.api.PetGoapi.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +11,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.PetGoapi.DTO.request.ClienteDto;
-import com.api.PetGoapi.DTO.request.EnderecoDtoRequest;
+import com.api.PetGoapi.Assembler.AnimalAssembler;
+import com.api.PetGoapi.Assembler.ClienteAssembler;
+import com.api.PetGoapi.Assembler.EnderecoAssembler;
+//import com.api.PetGoapi.DTO.request.ClienteInput;
 import com.api.PetGoapi.model.Animal;
 //import com.api.PetGoapi.Assembler.ClienteAssembler;
 import com.api.PetGoapi.model.Cliente;
 import com.api.PetGoapi.model.Endereco;
+import com.api.PetGoapi.model.Output.ClienteOutput;
+import com.api.PetGoapi.model.input.ClienteInput;
+import com.api.PetGoapi.repository.ClienteRepository;
 import com.api.PetGoapi.service.AnimalService;
 import com.api.PetGoapi.service.ClienteService;
 import com.api.PetGoapi.service.EnderecoService;
@@ -27,41 +31,46 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/")
-public class UsuarioController {
+public class ClienteController {
 	
 	//private ClienteAssembler clienteAssembler;
 	@Autowired
 	private ClienteService clienteService;
 	private AnimalService animalService;
 	private EnderecoService enderecoService;
+	private ClienteAssembler clienteAssembler; 
+	private EnderecoAssembler enderecoAssembler;
+	private AnimalAssembler animalAssembler;
+	private ClienteRepository clienteRepository;
 	//private EnderecoDtoRequest enderecoDtoRequest;
-	
-	@PostMapping
-	public ResponseEntity<Cliente> cadastrar(@RequestBody ClienteDto clienteDto /*ClienteInput clienteInput*/){
+@PostMapping	
+public ResponseEntity<Cliente> cadastrar(@RequestBody ClienteInput clienteInput /*ClienteInput clienteInput*/){
 		
-		if(!clienteDto.getSenha().contentEquals(clienteDto.getConfirmaSenha())) {
+	/*	if(!clienteDto.getSenha().contentEquals(clienteDto.getConfirmaSenha())) {
 			return ResponseEntity.badRequest().build();
-		}
-		Cliente clienteSalvo = clienteService.save(clienteDto.build()) ;
-		Animal animalSalvo = clienteDto.buildAnimal(clienteDto.getAnimais().get(0));
-		animalSalvo.setCliente(clienteSalvo);
-		animalService.cadastrar(animalSalvo);
-		EnderecoDtoRequest enderecoDtoRequest = new EnderecoDtoRequest();
-		Endereco enderecoSalvo = enderecoDtoRequest.build(clienteDto.getEndereco());
-		enderecoSalvo.setCliente(clienteSalvo);
-		enderecoService.cadastrar(enderecoSalvo);
-		
-		
-		return ResponseEntity.ok(clienteSalvo);
+		} */
+	Cliente newCliente = clienteService.save(clienteAssembler.toEntity(clienteInput));
+	Endereco newEndereco = enderecoAssembler.toEntity(clienteInput.getEndereco()); 
+	newEndereco.setCliente(newCliente);
+	enderecoService.cadastrar(newEndereco);
+	Animal newAnimal = animalAssembler.toEntity(clienteInput.getAnimal());
+	newAnimal.setCliente(newCliente);
+	animalService.cadastrar(newAnimal);
+	
+		return ResponseEntity.ok(newCliente);
 	}
 	@GetMapping("/usuarios")
-	public ResponseEntity<List<Cliente>> listar(){
-		return ResponseEntity.ok(clienteService.listar());
+	public List<ClienteOutput> listar(){
+		return clienteAssembler.toCollectionModel(clienteRepository.findAll());
+	
 	}
 	
 	@GetMapping("{clienteId}")
-	public ResponseEntity<Optional<Cliente>> buscar(@PathVariable Long clienteId){
+	public ResponseEntity<ClienteOutput> buscar(@PathVariable Long clienteId){
+		return clienteRepository.findById(clienteId)
+		.map(cliente -> ResponseEntity.ok(clienteAssembler.toModel(cliente)))
+		.orElse(ResponseEntity.notFound().build());
 		
-		return ResponseEntity.ok(clienteService.buscar(clienteId));
+
 	}
 }
